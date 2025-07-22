e4913e6a88dca6182830eb9aed86cabf26cd1dd7
kylefischer <72110505+kylefischer@users.noreply.github.com>
2025-07-22 15:12:05 -0700

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === 'production' ? "/resume-website/" : "/",
+  base: mode === 'production' ? "/structured-resume-night/" : "/",
 }));
